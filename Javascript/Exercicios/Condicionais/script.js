df08df74b8cbf24cc47fedb27060be5c66e86bcf
Guilherme <45200933+cguiama@users.noreply.github.com>
2022-06-27 23:40:31 -0300

    teste de condicionais

---
+++ Javascript/Exercicios/Condicionais/script.js
@@ -0,0 +1,49 @@
+let piloteName = prompt("Qual é o nome do piloto");
+let velNave = 0;
+let acelerar = prompt("Qual a velocidade de aceleração?");
+
+let confirma = confirm("Você confirma a velocidade de aceleração? " + acelerar);
+
+function aceleracao() {
+    while (confirma == false) {
+        acelerar = prompt("Qual a velocidade de aceleração?");
+        confirma = confirm("Você confirma a velocidade de aceleração? " + acelerar);
+    }
+
+    velNave = acelerar;
+
+    return confirma;
+}
+
+if (aceleracao() == true) {
+
+    if (velNave <= 0) {
+        while (velNave <= 0) {
+            confirma = false
+            alert("A nave está parada, considere acelerar");
+            aceleracao();
+        }
+    }
+    else if (velNave <= 40) {
+        while (velNave <= 40) {
+            confirma = false
+            alert("Velocidade lenta. Podemos acelerar um pouco mais.");
+            aceleracao();
+        }
+    }
+
+    else {
+        while (velNave >= 81) {
+            confirma = false
+            alert("Velocidade perigosa. Controle automatico forçado.\nVocê precisa digitar uma velocidade segura");
+            aceleracao();
+        }
+        if (velNave <= 80) {
+            alert("A nave está numa boa velocidade");
+        }
+        else {
+            confirma = false;
+        }
+    }
+
+}
---
+++ Javascript/Exercicios/script.js
@@ -0,0 +1,12 @@
+let idadevelha = prompt("Qual a idade da pessoa mais velha");
+let idadenova = prompt("Qual a idade da pessoa mais nova");
+
+let nomevelha = prompt("Qual o nome da pessoa mais velha");
+let nomenova = prompt("Qual o nome da pessoa mais nova");
+
+let diferenca = (idadevelha - idadenova);
+
+alert("A pessoa mais velha é: " + nomevelha + " sua idade é: " +idadevelha+
+"\nA pessoa mais nova é: " + nomenova + " sua idade é: " + idadenova + 
+"\nA diferença entre suas idades é: " + diferenca + "anos"
+);
