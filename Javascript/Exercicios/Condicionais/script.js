050fa5e6b8318dfd9063ea7fed02ed9f1187e3f0
Guilherme Castro <45200933+cguiama@users.noreply.github.com>
2023-05-08 14:55:09 -0300

    retomada de estudos

--- Javascript/Exercicios/Condicionais/script.js
+++ Javascript/Exercicios/Condicionais/script.js
@@ -1,46 +1,8 @@
-let piloteName = prompt("Qual é o nome do piloto");
-let velNave = 0;
-let acelerar = prompt("Qual a velocidade de aceleração?");
+let m, n, k
 
-let confirma = confirm("Você confirma a velocidade de aceleração? " + acelerar);
 
-function aceleracao() {
-    while (confirma == false) {
-        acelerar = prompt("Qual a velocidade de aceleração?");
-        confirma = confirm("Você confirma a velocidade de aceleração? " + acelerar);
-    }
-
-    velNave = acelerar;
-
-    return confirma;
-}
-
-if (aceleracao() == true) {
-
-    if (velNave <= 0) {
-        while (velNave <= 0) {
-            confirma = false
-            alert("A nave está parada, considere acelerar");
-            aceleracao();
-        }
-    }
-    else if (velNave <= 40) {
-        while (velNave <= 40) {
-            confirma = false
-            alert("Velocidade lenta. Podemos acelerar um pouco mais.");
-            aceleracao();
-        }
-    }
-
-    else {
-        while (velNave >= 81) {
-            confirma = false
-            alert("Velocidade perigosa. Controle automatico forçado.\nVocê precisa digitar uma velocidade segura");
-            aceleracao();
-        }
-        if (velNave <= 80) {
-            alert("A nave está numa boa velocidade");
-        }
-    }
-
+for (k = 1; k < 4; k++) {
+    m <= m - 1
+    n <= n + 1
+    alert(m, n)
 }
