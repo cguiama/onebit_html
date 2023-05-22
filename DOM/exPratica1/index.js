3e21cd8ec56674aa3fe224b549f0d324dee24059
Guilherme Castro <45200933+cguiama@users.noreply.github.com>
2023-05-22 19:41:51 -0300

    manipulação de DOM

---
+++ DOM/exPratica1/index.js
@@ -0,0 +1,50 @@
+function addContact() {
+	const contactSection = document.getElementById("contactsList");
+
+	const h3 = document.createElement("h3");
+	h3.innerText = "Contato";
+
+	const ul = document.createElement("ul");
+
+	const nameLi = document.createElement("li");
+	nameLi.innerHTML = "Nome: ";
+	const nameInput = document.createElement("input");
+	nameInput.type = "text";
+	nameInput.name = "fullname";
+	nameLi.appendChild(nameInput);
+	ul.appendChild(nameLi);
+	ul.appendChild(document.createElement("br"));
+
+	const phoneLi = document.createElement("li");
+	phoneLi.innerText = "Telefone: ";
+	const phoneInput = document.createElement("input");
+	phoneInput.type = "text";
+	phoneInput.name = "phone";
+	phoneLi.appendChild(phoneInput);
+	ul.appendChild(phoneLi);
+	//a função document.createElement('tipo de tag') é utilizada para criar uma tag (elemento) dentro do html, dentro de ('') passamos o tipo
+	// na linha abaixo criamos o elemento <br>
+	ul.appendChild(document.createElement("br"));
+
+	const addresLi = document.createElement("li");
+	addresLi.innerHTML = '<label for="address">Endereço: </label>';
+	const addressInput = document.createElement("input");
+	addressInput.type = "text";
+	addressInput.name = "address";
+	addressInput.id = "address";
+	addresLi.appendChild(addressInput);
+	ul.appendChild(addresLi);
+	ul.appendChild(document.createElement("br"));
+
+	contactSection.append(h3, ul);
+}
+
+function removeContact() {
+	const contactSection = document.getElementById("contactsList");
+
+	const titles = document.getElementsByTagName("h3");
+	const contacts = document.getElementsByTagName("ul");
+
+	contactSection.removeChild(titles[titles.length - 1]);
+	contactSection.removeChild(contacts[contacts.length - 1]);
+}
