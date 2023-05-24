db1bf2a9c3d82a5a6db158b9e087abb66af888b3
Guilherme Castro <45200933+cguiama@users.noreply.github.com>
2023-05-24 14:51:39 -0300

    trabalhando com eventos

---
+++ DOM/exPratica1/eventos.js
@@ -0,0 +1,12 @@
+/*
+
+- selecionar o elementiD orderForm
+- criar a função addEventListener do tipo submit e passar uma função que indica um evento (ev)
+- selecionar os eventos e receber seus valores
+- selecionar e receber todos os valores do checkbox
+- retornar tudo no console.log
+- usar preventDefault() para impedir o comportamento padrão do evento de envio do formulario.
+- atribuir valores vazios ao form
+- criar um alert para retornar as frases do pedido realizado e passar os itens que vão no sanduiche
+
+*/
--- DOM/exPratica1/index.js
+++ DOM/exPratica1/index.js
@@ -1,52 +1,14 @@
-function addContact() {
-	const contactSection = document.getElementById("contactsList");
+function register(element) {
+	const username = element.children.username.value;
+	const password = element.children.password.value;
 
-	const h3 = document.createElement("h3");
-	h3.innerText = "Contato";
+	const passwordConfirmation = element.children.passwordConfirmation.value;
 
-	const ul = document.createElement("ul");
+	if (password === passwordConfirmation) {
+		alert("Usuário registrado com sucesso.");
+	} else {
+		alert("As senhas não conferem");
+	}
 
-	const nameLi = document.createElement("li");
-	nameLi.innerHTML = "Nome: ";
-	const nameInput = document.createElement("input");
-	nameInput.type = "text";
-	nameInput.name = "fullname";
-	nameLi.appendChild(nameInput);
-	ul.appendChild(nameLi);
-	ul.appendChild(document.createElement("br"));
-
-	const phoneLi = document.createElement("li");
-	phoneLi.innerText = "Telefone: ";
-	const phoneInput = document.createElement("input");
-	phoneInput.type = "text";
-	phoneInput.name = "phone";
-	phoneLi.appendChild(phoneInput);
-	ul.appendChild(phoneLi);
-	//a função document.createElement('tipo de tag') é utilizada para criar uma tag (elemento) dentro do html, dentro de ('') passamos o tipo
-	// na linha abaixo criamos o elemento <br>
-	ul.appendChild(document.createElement("br"));
-
-	const addresLi = document.createElement("li");
-	addresLi.innerHTML = '<label for="address">Endereço: </label>';
-	const addressInput = document.createElement("input");
-	addressInput.type = "text";
-	addressInput.name = "address";
-	addressInput.id = "address";
-	addresLi.appendChild(addressInput);
-	ul.appendChild(addresLi);
-	ul.appendChild(document.createElement("br"));
-
-	contactSection.append(h3, ul);
-}
-
-function removeContact() {
-	const contactSection = document.getElementById("contactsList");
-
-	const titles = document.getElementsByTagName("h3");
-	const contacts = document.getElementsByTagName("ul");
-
-	//Nesta função, criamos o removeChild passando as tags que foram criandas com o append, sempre na ultima posição da Lista
-
-	contactSection.removeChild(titles[titles.length - 1]);
-	contactSection.removeChild(contacts[contacts.length - 1]);
+	console.log(username, password, passwordConfirmation);
 }
